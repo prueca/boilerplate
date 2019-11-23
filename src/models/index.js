85219c02068dc298101f30d90fa7c14b20e78355
Peter John R. Rueca <peterrueca@yahoo.com>
2019-11-23 17:37:59 +0800

    Added /models

---
+++ src/models/index.js
@@ -0,0 +1,25 @@
+import Sequelize from 'sequelize';
+import {
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
+  DB_HOST,
+  DB_PORT,
+  DB_DIALECT
+} from '../configs/app';
+
+export const { Op } = Sequelize;
+export const sequelize = new Sequelize(
+  DB_NAME, DB_USER, DB_PASS, {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: DB_DIALECT
+  });
+
+const models = {};
+
+if (process.env.DB_SYNC == 1) {
+  sequelize.sync({ force: true });
+}
+
+export default models;
