d501d01616a10aa6b2e15d9bfeba757988406eef
Peter John Rueca <peterrueca@yahoo.com>
2020-02-01 22:53:08 +0800

    added helper and library directory

--- src/configs/app.js
+++ src/configs/app.js
@@ -1,10 +1,12 @@
+import { env } from '../library/helper';
+
 export const
-  APP_NAME = process.env.APP_NAME || 'App',
-  BASE_URL = process.env.BASE_URL || 'http://localhost:3000',
-  PORT = process.env.PORT || 3000,
-  DB_NAME = process.env.DB_NAME || 'database',
-  DB_USER = process.env.DB_USER || 'root',
-  DB_PASS = process.env.DB_PASS || '',
-  DB_HOST = process.env.DB_HOST || 'localhost',
-  DB_PORT = process.env.DB_PORT || 3306,
-  DB_DIALECT = process.env.DB_DIALECT || 'mysql';
+  APP_NAME = env('APP_NAME', 'App'),
+  BASE_URL = env('BASE_URL', 'http://localhost:3000'),
+  PORT = env('PORT', 3000),
+  DB_NAME = env('DB_NAME', 'database'),
+  DB_USER = env('DB_USER', 'root'),
+  DB_PASS = env('DB_PASS', ''),
+  DB_HOST = env('DB_HOST', 'localhost'),
+  DB_PORT = env('DB_PORT', 3306),
+  DB_DIALECT = env('DB_DIALECT', 'mysql');
